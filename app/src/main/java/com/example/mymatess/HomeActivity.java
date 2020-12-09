a4defb60d765d9088a94d086f0125b157bba7aab
Nadav Shalev <nadav.shalev@fyber.com>
2020-12-09 15:57:01 +0200

    asdasd

--- app/src/main/java/com/example/mymatess/HomeActivity.java
+++ app/src/main/java/com/example/mymatess/HomeActivity.java
@@ -6,24 +6,37 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
-    private FirebaseAuth mAuth;
+    private RecyclerView mRecyclerView;
+    private RecyclerViewDates mAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.home_activity);
+        initRecyclerView();
     }
 
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    private void initRecyclerView() {
+        final ArrayList<Hobbie> hobbies = new ArrayList();
+        mRecyclerView = findViewById(R.id.recycler_view_dates);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        mAdapter = new RecyclerViewDates(this, mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 }
---
+++ app/src/main/java/com/example/mymatess/RecyclerViewDates.java
@@ -0,0 +1,98 @@
+package com.example.mymatess;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewDates extends RecyclerView.Adapter<RecyclerViewDates.ViewHolder> {
+
+    private List<Date> mData = new ArrayList<>();
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private RecyclerView mRecyclerView;
+
+
+    public RecyclerViewDates(Context context, RecyclerView recyclerView) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mRecyclerView = recyclerView;
+        mData.add(new Date(9, 12, 20, true));
+        mData.add(new Date(10, 12, 20, false));
+        mData.add(new Date(11, 12, 20, false));
+        mData.add(new Date(12, 12, 20, false));
+        mData.add(new Date(13, 12, 20, false));
+        mData.add(new Date(14, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mData.add(new Date(15, 12, 20, false));
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = mInflater.inflate(R.layout.square_day, parent, false);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                int itemPosition = mRecyclerView.getChildLayoutPosition(v);
+                for (Date date : mData) {
+                    date.mIsChecked = false;
+                }
+                mData.get(itemPosition).mIsChecked = true;
+                notifyDataSetChanged();
+            }
+        });
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Date date = mData.get(position);
+        if (date.mIsChecked) {
+            holder.date.setTextColor(Color.WHITE);
+            holder.relativeLayout.setBackgroundResource(R.color.green);
+        } else {
+            holder.date.setTextColor(Color.GREEN);
+            holder.relativeLayout.setBackgroundResource(R.color.white);
+        }
+        holder.date.setText(String.format("%d/%d", date.day, date.month));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView date;
+        RelativeLayout relativeLayout;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            relativeLayout = itemView.findViewById(R.id.relative_layout_date);
+            date = itemView.findViewById(R.id.date);
+        }
+
+    }
+
+    public void setNewList(List<Date> data) {
+        mData = data;
+        notifyDataSetChanged();
+    }
+}
