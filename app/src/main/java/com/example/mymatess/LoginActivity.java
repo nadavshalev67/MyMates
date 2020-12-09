de0df104b29a3bfe5ddc869ec7a87acfef27bfe3
Rosenthal <Ephraim.Rosenthal@fyber.com>
2020-12-09 14:34:06 +0200

    sign in

--- app/src/main/java/com/example/mymatess/LoginActivity.java
+++ app/src/main/java/com/example/mymatess/LoginActivity.java
@@ -2,19 +2,31 @@ package com.example.mymatess;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     EditText mEmail, mPassword;
     TextView mForgotYourPassword;
     Button mLogin, mRegister;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -28,8 +40,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mRegister = findViewById(R.id.register_button);
         mRegister.setOnClickListener(this);
         mForgotYourPassword = findViewById(R.id.forgot_yourt_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+    */
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
 
     @Override
     public void onClick(View view) {
@@ -37,7 +72,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.login_button: {
                  String email= mEmail.getText().toString();
                  String password= mPassword.getText().toString();
-                 
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(password, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                                   // updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(password, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                    // ...
+                                }
+
+                                // ...
+                            }
+                        });
+
+
                 break;
             }
             case R.id.forgot_yourt_password: {
