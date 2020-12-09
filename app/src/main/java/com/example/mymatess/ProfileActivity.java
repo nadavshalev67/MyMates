20129deb7f020eabf64f99a730937016555307de
Nadav Shalev <nadav.shalev@fyber.com>
2020-12-09 13:37:42 +0200

    blabla

--- app/src/main/java/com/example/mymatess/ProfileActivity.java
+++ app/src/main/java/com/example/mymatess/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.example.mymatess;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,5 +26,21 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
 
         }
+    }
+
+    private void initSpinner() {
+        final String[] Departments = new String[]{"blabla", "blabla"};//this come from firebase!!
+        final Spinner spinner = findViewById(R.id.spinner_codespinner);
+        spinner.setAdapter(new ArrayAdapter<String>(this, R.layout.spinner_layout, R.id.textview, Departments));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinner.setSelection(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mymatess/RegisterActivity.java
+++ app/src/main/java/com/example/mymatess/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.mymatess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mContinueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        mContinueButton = findViewById(R.id.continue_button);
 
     }
 
@@ -22,7 +25,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
+            case R.id.continue_button: {
+                Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+            }
 
         }
     }
